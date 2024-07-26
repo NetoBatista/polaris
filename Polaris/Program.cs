336fa9de64b2a5314aa95f369631fa899fa9a1c1
Neto Batista <netosantosbatista@outlook.com>
2024-07-26 08:09:28 -0300

    Remove https in localhost and update nuget swagger

--- Polaris/Program.cs
+++ Polaris/Program.cs
@@ -19,9 +19,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseHttpsRedirection();
+}
 
-app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
